615d63b62f0e75812128be09213e59e75e57063f
Saitest <91127281+saiotest@users.noreply.github.com>
2023-05-14 17:09:04 -0300

    Initial commit

---
+++ src/models/bankaccount.ts
@@ -0,0 +1,13 @@
+export interface BankAccount {
+	id: string;
+	uuid: string;
+	userId: string;
+	bankName: string;
+	accountNumber: string;
+	routingNumber: string;
+	isDeleted: boolean;
+	createdAt: Date;
+	modifiedAt: Date;
+}
+
+export type BankAccountPayload = Pick<BankAccount, 'userId' | 'bankName' | 'accountNumber' | 'routingNumber'>;
